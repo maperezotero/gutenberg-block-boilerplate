e53d3eb37b085f20c6beb9bd703f038c61a1df81
maperezotero <maperezotero@gmail.com>
2022-02-22 19:01:14 +0100

    First commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '74bea92f4e65dbbf050acaca5e676e53');
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { registerBlockType } from '@wordpress/blocks';
+import './style.scss';
+
+import Edit from './edit';
+import save from './save';
+
+registerBlockType('create-block/boilerplate', {
+	edit: Edit,
+	save,
+});
